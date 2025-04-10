38272f39b186f43af6b117848a26e8548c4b440c
delaureanlaura@hotmail.com <delaureanlaura@hotmail.com>
2025-04-10 09:40:07 -0300

    feat: adiciona classe EstoqueService com métodos principais

---
+++ OneDrive/Área de Trabalho/Programas Java/atividades/Atividade 1/ProjetoDeProgramacao/src/estoqueService.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class estoqueService {
+
+        public void adicionarProduto(produto p){
+            produtos.put(p.getId(), p);
+
+        }
+
+        public void removerProduto(String id) {
+            produto.remove(id);
+
+        }
+
+        public list<produto> listarProdutos(){
+
+        }
+
+        public produto buscarProduto(string id){
+
+        }
+
+        atualizarqtd (string id, int novaQtd){
+
+        }
+
+        verificarEstoque(String id, int qtd){
+
+        }
+
+
+
+
+
+}
---
+++ OneDrive/Área de Trabalho/Programas Java/atividades/Atividade 1/ProjetoDeProgramacao/src/vendaService.java
@@ -0,0 +1,17 @@
+public class vendaService {
+    private estoqueService estoqueService;
+
+    public VendaService(estoqueService estoqueService){
+        this.estoqueService = estoqueService;
+    }
+
+    public Venda registrarVenda(List<ItemVenda> itens) {
+
+    }
+    public double CalcularTotal(List<ItemVenda> itens) {
+
+    }
+    public void atualizarEstoque(List<ItemVenda> itens) {
+
+    }
+}
